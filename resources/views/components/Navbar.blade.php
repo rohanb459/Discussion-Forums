abf4e03507cf4ed804a08e6230facbc4e4dac1dc
Rohan Bhatia <rbhatia@avaloninfosys.com>
2024-05-10 17:22:32 +0530

    add post -> post query

--- resources/views/components/Navbar.blade.php
+++ resources/views/components/Navbar.blade.php
@@ -7,7 +7,7 @@
         <a href="{{route('home')}}" class="navbar__link"><i data-feather="home"></i><span>Home</span></a>
       </li>
       <li class="navbar__item">
-        <a href="{{route('create-post')}}" class="navbar__link"><i data-feather="message-square"></i><span>Add Post</span></a>
+        <a href="{{route('create-post')}}" class="navbar__link"><i data-feather="message-square"></i><span>Post Query</span></a>
       </li>
       {{-- <li class="navbar__item">
         <a href="#" class="navbar__link"><i data-feather="users"></i><span>Customers</span></a>
